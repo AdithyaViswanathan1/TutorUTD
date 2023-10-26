41d373ea8a3e84f4efb2430c309f62b14bca5a1e
Adithya Viswanathan <adithya@Adithyas-Air.attlocal.net>
2023-10-26 10:43:48 -0500

    tutor views change again

--- back/tutor/views.py
+++ back/tutor/views.py
@@ -23,21 +23,3 @@ Tutor.objects.raw(query)
 
 for p in Tutor.objects.raw("SELECT * FROM tutor"):
     print("tutor::",p.full_name,p.email)
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Tutor
-
-def say_hello(request):
-    return HttpResponse('Hello world! This is the login endpoint.')
-
-# run command "python3 manage.py makemigrations" to execute this sql query
-# query to add tutor
-#name = input("name:")
-#email = input("email:")
-#password = input("password:")
-#test = Tutor(full_name=name, email=email, password=password)
-#test.save()
-
-# RAW SQL COMMAND EXAMPLE
-# for p in Tutor.objects.raw("SELECT * FROM tutor"):
-#     print("tutor::",p)
