f345baafc6c1c156d2c759a201bc5f56a423ce44
Adithya Viswanathan <adithya@Adithyas-Air.attlocal.net>
2023-10-30 17:31:46 -0500

    tutordetail view (individual tutor get,put,delete) working

--- back/tutor/urls.py
+++ back/tutor/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from tutor import views
-from tutor.views import TutorList, TutorCreate
+from tutor.views import TutorList, TutorCreate, TutorDetail
 
 urlpatterns = [
     path('', views.say_hello),
     path('list/', TutorList.as_view()),
+    path("list/<int:pk>", TutorDetail.as_view()),
     path('create/', TutorCreate.as_view()),
 ]
--- back/tutor/views.py
+++ back/tutor/views.py
@@ -26,6 +26,31 @@ class TutorCreate(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class TutorDetail(APIView):
+    def get_tutor_by_pk(self, pk):
+        try:
+            movie = Tutor.objects.get(pk=pk)
+            return movie
+        except Tutor.DoesNotExist:
+            return Response({"Error": "Movie Does Not Exist"},status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        movie = self.get_tutor_by_pk(pk)
+        serializer = TutorSerializer(movie)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        movie = self.get_tutor_by_pk(pk)
+        serializer = TutorSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        movie = self.get_tutor_by_pk(pk)
+        movie.delete()
+        return Response({"Response": "Successfully deleted record"},status=status.HTTP_204_NO_CONTENT)
 
 # run command "python3 manage.py makemigrations" to execute this sql query
 # query to add tutor
