465ce43b78e1e7698f63699598d84bbd1084479e
Adithya Viswanathan <adithya@Adithyas-Air.attlocal.net>
2023-10-30 18:01:42 -0500

    tutor validations in serializer.py

--- back/tutor/serializers.py
+++ back/tutor/serializers.py
@@ -12,8 +12,14 @@ class TutorSerializer(serializers.ModelSerializer):
     #     else:
     #         return data
     
-    # def validate_name(self,value):
-    #     if len(value) < 3:
-    #         raise serializers.ValidationError("Name is too short!")
-    #     else:
-    #         return value
+    def validate_email(self,value):
+        if "@" not in value:
+            raise serializers.ValidationError("ERROR: Email address not valid")
+        else:
+            return value
+    
+    def validate_full_name(self,value):
+        if " " not in value:
+            raise serializers.ValidationError("ERROR: Full Name should include first and last name separated by a space")
+        else:
+            return value
