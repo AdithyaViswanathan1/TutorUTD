e768bc606d395e70cbef0072a78479547d900f54
vtranbui <vtranbui@gmail.com>
2023-12-05 12:28:05 -0600

    Added gmail sending account, fixed some bugs

--- back/online_tutoring/cron.py
+++ back/online_tutoring/cron.py
@@ -1,30 +1,42 @@
+from django.core import mail
+from django.conf import settings
 from tutor.models import Tutor
 from student.models import Student
 from appointments.models import Appointments
 
 def dailyEmailReminder():
-    if studentapp = appointments.objects.filter(time=datetime.date.today()).order_by("student"):
-        currentStudentId = studentapp.first().student
-        for e in studentapp:
-            message = "This is a reminder from TutorUTD that you have the following tutoring appointments today: \n" 
-            if e.student==currentStudentId:
-                message + "Appointment at time " + e.time + " for course " + e.course + " at " + e.location + " with tutor " Tutor.objects.get(e.tutor).full_name + "\n"
-            else:
-                send_mail("Appointment Reminder",
-                    message,
-                    "FROM@EMAIL",
-                    [Student.objects.get(pk=currentStudentId).email])
-                currentStudentId = e.student
+    connection = get_connection() #Establishes connection to mail server
     
-    if tutorapp = appointments.objects.filter(time=datetime.date.today()).order_by("tutor"):
-        currentTutorId = tutorapp.first().tutor
-        for e in tutorapp:
-            if e.tutor==currentTutorId:
-                message + "Appointment at time " + e.time + " for course " + e.course + " at " + e.location + " with student " Student.objects.get(e.tutor).full_name
-            else:
+    #Send email to all students with appointment matching today's date
+    if studentappointments = Appointments.objects.filter(time=datetime.date.today()).order_by("student"):
+        currentStudentId = studentappointments.first().student
+        message = "This is a reminder from TutorUTD that you have the following tutoring appointments today: \n \n" 
+        for e in studentappointments:
+            if e.student!=currentStudentId:
                 send_mail("Appointment Reminder",
                     message,
-                    "FROM@EMAIL",
+                    settings.EMAIL_HOST_USER,
+                    [Student.objects.get(pk=currentStudentId).email], 
+                    connection)
+                currentStudentId = e.student
+                message = "This is a reminder from TutorUTD that you have the following tutoring appointments today: \n \n" 
+                
+            message + " Appointment at time " + e.time + " for course " + e.course + " at " + e.location + " with tutor " Tutor.objects.get(e.tutor).full_name + "\n"
+    
+    #Send email to all tutors with appointment matching today's date
+    if tutorappointments = Appointments.objects.filter(time=datetime.date.today()).order_by("tutor"):
+        currentTutorId = tutorappointments.first().tutor
+        message = "This is a reminder from TutorUTD that you have the following tutoring appointments today: \n \n" 
+        for e in tutorappointments:
+            if e.tutor!=currentTutorId:
+                send_mail("Appointment Reminder",
+                    message,
+                    settings.EMAIL_HOST_USER,
                     [Tutor.objects.get(pk=currentTutorId).email], 
-                    get_connection())
+                    connection)
                 currentTutorId = e.tutor
+                message = "This is a reminder from TutorUTD that you have the following tutoring appointments today: \n \n" 
+                
+            message + " Appointment at time " + e.time + " for course " + e.course + " at " + e.location + " with student " Student.objects.get(e.tutor).full_name + "\n"
+    
+    connection.close()
